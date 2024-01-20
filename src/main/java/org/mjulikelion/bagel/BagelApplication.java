2c3f35b4e513d612a285ab4599cd82cc20562d94
Kim Daehyeon <daehyeon3351@gmail.com>
2024-01-21 03:23:04 +0900

    Init Spring Boot Application
    - .gitignore
    - .gitmessage.txt
    - README

---
+++ src/main/java/org/mjulikelion/bagel/BagelApplication.java
@@ -0,0 +1,13 @@
+package org.mjulikelion.bagel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagelApplication.class, args);
+	}
+
+}
