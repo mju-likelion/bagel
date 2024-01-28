1116625c94cb2a4dacfe3d949708ab70c6ed7c5d
Kim Daehyeon <daehyeon3351@gmail.com>
2024-01-29 04:46:25 +0900

    fix: id 컬럼 자료형 변경 - #10
    - UUID to String
    - UUID 호환성 문제 때문

--- src/main/java/org/mjulikelion/bagel/model/Introduce.java
+++ src/main/java/org/mjulikelion/bagel/model/Introduce.java
@@ -5,12 +5,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import java.util.List;
-import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,8 +21,7 @@ import lombok.NoArgsConstructor;
 public class Introduce {
     @Id
     @Column(updatable = false, unique = true, nullable = false)
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "uuid2")
-    private UUID id;
+    private String id;
 
     @Column(nullable = false, length = 100)
     private String title;
--- src/main/java/org/mjulikelion/bagel/repository/AgreementRepository.java
+++ src/main/java/org/mjulikelion/bagel/repository/AgreementRepository.java
@@ -1,8 +1,7 @@
 package org.mjulikelion.bagel.repository;
 
-import java.util.UUID;
 import org.mjulikelion.bagel.model.Agreement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AgreementRepository extends JpaRepository<Agreement, UUID> {
+public interface AgreementRepository extends JpaRepository<Agreement, String> {
 }
