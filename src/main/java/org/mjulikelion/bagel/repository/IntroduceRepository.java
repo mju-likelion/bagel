4198a566188341eeb3252b0ff77e3cab66daf5d8
Kim Daehyeon <daehyeon3351@gmail.com>
2024-01-28 23:21:46 +0900

    feat: findAllByPartOrderBySequence 구현 - #5
    - 해당 Part의 모든 Introduce를 찾은 후 Sequence 컬럼으로 정렬 후 반환

--- src/main/java/org/mjulikelion/bagel/repository/IntroduceRepository.java
+++ src/main/java/org/mjulikelion/bagel/repository/IntroduceRepository.java
@@ -1,8 +1,11 @@
 package org.mjulikelion.bagel.repository;
 
+import java.util.List;
 import java.util.UUID;
 import org.mjulikelion.bagel.model.Introduce;
+import org.mjulikelion.bagel.model.Part;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IntroduceRepository extends JpaRepository<Introduce, UUID> {
+    List<Introduce> findAllByPartOrderBySequence(Part part);
 }
