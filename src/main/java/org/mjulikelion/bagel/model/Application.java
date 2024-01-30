f571474077e58666b1679e2bd62fa0424b60cf95
Kim Daehyeon <daehyeon3351@gmail.com>
2024-01-30 11:21:59 +0900

    feat: Application과 Major의 관계 설정 - #12
    - Application과 Major의 관계 설정을 통해 하나의 Major가 여러 개의 Application을 가질 수 있도록 변경
    - Application에 ManyToOne 설정
    - Major에 OneToMany 설정

--- src/main/java/org/mjulikelion/bagel/model/Application.java
+++ src/main/java/org/mjulikelion/bagel/model/Application.java
@@ -8,8 +8,9 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
@@ -34,7 +35,8 @@ public class Application {
     @Column(length = 100)
     private String name;
 
-    @OneToOne(targetEntity = Major.class)
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "major_id")
     private Major major;
 
     @Column(name = "student_id", length = 100)
