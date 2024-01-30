6f782c05d27db88f8f4620611697155a0bfe5fe4
Kim Daehyeon <daehyeon3351@gmail.com>
2024-01-30 14:11:49 +0900

    feat: Part 별 자기소개 항목 수를 세는 함수 추가 - #9
    - Service 에서 사용 예정

--- src/main/java/org/mjulikelion/bagel/repository/IntroduceRepository.java
+++ src/main/java/org/mjulikelion/bagel/repository/IntroduceRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IntroduceRepository extends JpaRepository<Introduce, String> {
     List<Introduce> findAllByPartOrderBySequence(Part part);
+
+    Long countByPart(Part part);
 }
