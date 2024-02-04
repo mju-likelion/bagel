92bdef35d32fd43378bd655c57c6368d7928c2ed
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-04 19:20:51 +0900

    refactor: 필요 없는 @JsonProperty 제거 - #32

--- src/main/java/org/mjulikelion/bagel/dto/response/apply/ApplyExistResponseData.java
+++ src/main/java/org/mjulikelion/bagel/dto/response/apply/ApplyExistResponseData.java
@@ -1,10 +1,8 @@
 package org.mjulikelion.bagel.dto.response.apply;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
 @Builder
 public class ApplyExistResponseData {
-    @JsonProperty("isExist")
     private Boolean isExist;
 }
