afe8d177fb91dacba565a44af9d121101e0bb9fa
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-11 22:11:01 +0900

    feat: 지원서 get 요청에 대한 응답에 전공 추가 - #34

--- src/main/java/org/mjulikelion/bagel/dto/response/application/ApplicationGetResponseData.java
+++ src/main/java/org/mjulikelion/bagel/dto/response/application/ApplicationGetResponseData.java
@@ -2,14 +2,21 @@ package org.mjulikelion.bagel.dto.response.application;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import org.mjulikelion.bagel.model.Agreement;
 import org.mjulikelion.bagel.model.Introduce;
+import org.mjulikelion.bagel.model.Major;
 
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApplicationGetResponseData {
     @JsonProperty("introduces")
     private List<Introduce> introduces;
     @JsonProperty("agreements")
     private List<Agreement> agreements;
+    @JsonProperty("majors")
+    private List<Major> majors;
 }
