e35d1fa1c7c61e78f86ee2c6e4329dac4a92528e
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-11 21:40:09 +0900

    feat: validation 추가 - #34
    - Map에 있던 id들이 모두 DB에 있는지 검증

--- src/main/java/org/mjulikelion/bagel/util/converter/ApplicationAgreementConverter.java
+++ src/main/java/org/mjulikelion/bagel/util/converter/ApplicationAgreementConverter.java
@@ -1,5 +1,6 @@
 package org.mjulikelion.bagel.util.converter;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -7,15 +8,15 @@ import lombok.AllArgsConstructor;
 import org.mjulikelion.bagel.model.Application;
 import org.mjulikelion.bagel.model.ApplicationAgreement;
 import org.mjulikelion.bagel.repository.AgreementRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
-@Component
+@Service
 public class ApplicationAgreementConverter {
     private final AgreementRepository agreementRepository;
 
     /**
-     * Description: {@code Map<String, String>, Application -> List<ApplicationAgreement> 변환}
+     * {@code Map<String, String>, Application -> List<ApplicationAgreement> 변환}.
      *
      * @param agreementMap agreement 아이디, 동의 여부
      * @param application  application 객체
@@ -23,11 +24,27 @@ public class ApplicationAgreementConverter {
      */
     public List<ApplicationAgreement> convertMapToApplicationAgreement(Map<String, Boolean> agreementMap,
                                                                        Application application) {
-        return agreementMap.keySet().stream()
+        List<String> agreementIdList = new ArrayList<>(agreementMap.keySet());
+        if (!this.isValidAgreementMap(agreementIdList)) {
+            return null;
+        }
+
+        return agreementIdList.stream()
                 .map(agreementId -> ApplicationAgreement.builder()
-                        .agreement(agreementRepository.findById(agreementId).orElseThrow())
+                        .agreement(this.agreementRepository.findById(agreementId).orElseThrow())
                         .application(application)
                         .build())
                 .collect(Collectors.toList());
+    }
+
+    /**
+     * agreementIdList가 유효한지 확인.
+     *
+     * @param agreementIdList agreement 아이디 리스트
+     * @return 유효하면 true, 아니면 false
+     */
+    private boolean isValidAgreementMap(List<String> agreementIdList) {
+        return agreementIdList.stream()
+                .allMatch(this.agreementRepository::existsById);
     }
 }
--- src/main/java/org/mjulikelion/bagel/util/converter/ApplicationIntroduceConverter.java
+++ src/main/java/org/mjulikelion/bagel/util/converter/ApplicationIntroduceConverter.java
@@ -1,5 +1,6 @@
 package org.mjulikelion.bagel.util.converter;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -7,15 +8,15 @@ import lombok.AllArgsConstructor;
 import org.mjulikelion.bagel.model.Application;
 import org.mjulikelion.bagel.model.ApplicationIntroduce;
 import org.mjulikelion.bagel.repository.IntroduceRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
-@Component
+@Service
 public class ApplicationIntroduceConverter {
     private final IntroduceRepository introduceRepository;
 
     /**
-     * Description: {@code Map<String, String>, Application -> List<ApplicationIntroduce> 변환}
+     * {@code Map<String, String>, Application -> List<ApplicationIntroduce> 변환}
      *
      * @param introduceMap introduce 아이디, introduce 내용
      * @param application  application 객체
@@ -23,12 +24,27 @@ public class ApplicationIntroduceConverter {
      */
     public List<ApplicationIntroduce> convertMapToApplicationIntroduce(Map<String, String> introduceMap,
                                                                        Application application) {
-        return introduceMap.keySet().stream()
+        List<String> introduceList = new ArrayList<>(introduceMap.keySet());
+        if (!this.isValidIntroduceMap(introduceList)) {
+            return null;
+        }
+        return introduceList.stream()
                 .map(introduce -> ApplicationIntroduce.builder()
                         .introduce(this.introduceRepository.findById(introduce).orElseThrow())
                         .content(introduceMap.get(introduce))
                         .application(application)
                         .build())
                 .collect(Collectors.toList());
+    }
+
+    /**
+     * introduceList가 유효한지 확인
+     *
+     * @param introduceMap introduce 아이디 리스트
+     * @return 유효하면 true, 아니면 false
+     */
+    private boolean isValidIntroduceMap(List<String> introduceMap) {
+        return introduceMap.stream()
+                .allMatch(this.introduceRepository::existsById);
     }
 }
