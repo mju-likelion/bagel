8512a9c34993343c9ac053c45ec75b35d82d0c64
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-11 21:47:30 +0900

    refactor: 학번으로 지원함에 따른 쿼리스트링 변경 - #34

--- src/main/java/org/mjulikelion/bagel/controller/ApplyController.java
+++ src/main/java/org/mjulikelion/bagel/controller/ApplyController.java
@@ -1,12 +1,12 @@
 package org.mjulikelion.bagel.controller;
 
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.mjulikelion.bagel.dto.request.ApplySaveDto;
 import org.mjulikelion.bagel.dto.response.ResponseDto;
 import org.mjulikelion.bagel.dto.response.apply.ApplyExistResponseData;
 import org.mjulikelion.bagel.service.apply.ApplyCommandService;
 import org.mjulikelion.bagel.service.apply.ApplyQueryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,26 +16,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("apply")
 public class ApplyController {
-    private final ApplyQueryService applicationQueryService;
-    private final ApplyCommandService applicationCommandService;
+    private final ApplyQueryService applyQueryService;
+    private final ApplyCommandService applyCommandService;
 
-    @Autowired
-    public ApplyController(ApplyQueryService applicationQueryService, ApplyCommandService applicationCommandService) {
-        this.applicationQueryService = applicationQueryService;
-        this.applicationCommandService = applicationCommandService;
-    }
-
-    @GetMapping("/exist/{userId}")
+    @GetMapping("/exist/{studentId}")
     public ResponseEntity<ResponseDto<ApplyExistResponseData>> getApplicationExist(
-            @PathVariable("userId") String userId) {
-        return this.applicationQueryService.getApplyExist(userId);
+            @PathVariable("studentId") String studentId) {
+        return this.applyQueryService.getApplyExist(studentId);
     }
 
     @PostMapping()
     public ResponseEntity<ResponseDto<Void>> saveApply(
             @RequestBody @Valid ApplySaveDto applySaveDto) {
-        return this.applicationCommandService.saveApply(applySaveDto);
+        return this.applyCommandService.saveApply(applySaveDto);
     }
 }
