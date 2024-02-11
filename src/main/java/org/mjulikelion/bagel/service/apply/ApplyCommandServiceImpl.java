3cfa5f99ad19bddf9ef7110ef12f7e686638ab56
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-11 21:43:15 +0900

    refactor: 학번으로 지원함에 따른 지원 로직 변경 - #34
    - Application을 생성하지 않고 History 테이블에 학번을 저장
    - userId 대신 학번으로 중복 검사

--- src/main/java/org/mjulikelion/bagel/service/apply/ApplyCommandServiceImpl.java
+++ src/main/java/org/mjulikelion/bagel/service/apply/ApplyCommandServiceImpl.java
@@ -3,8 +3,9 @@ package org.mjulikelion.bagel.service.apply;
 import lombok.AllArgsConstructor;
 import org.mjulikelion.bagel.dto.request.ApplySaveDto;
 import org.mjulikelion.bagel.dto.response.ResponseDto;
-import org.mjulikelion.bagel.model.Application;
+import org.mjulikelion.bagel.model.History;
 import org.mjulikelion.bagel.repository.ApplicationRepository;
+import org.mjulikelion.bagel.repository.HistoryRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -14,21 +15,24 @@ import org.springframework.transaction.annotation.Transactional;
 @AllArgsConstructor
 public class ApplyCommandServiceImpl implements ApplyCommandService {
     private final ApplicationRepository applicationRepository;
+    private final HistoryRepository historyRepository;
 
     @Override
     @Transactional
     public ResponseEntity<ResponseDto<Void>> saveApply(ApplySaveDto applySaveDto) {
-        if (this.applicationRepository.existsByUserId(applySaveDto.getUserId())) {
+        //이미 지원서가 있는지 확인
+        if (this.applicationRepository.existsByStudentId(applySaveDto.getStudentId())) {
             return new ResponseEntity<>(ResponseDto.res(
                     HttpStatus.CONFLICT,
                     "Already applied"
             ), HttpStatus.CONFLICT);
         }
 
-        Application application = Application.builder()
-                .userId(applySaveDto.getUserId())
+        History history = History.builder()
+                .studentId(applySaveDto.getStudentId())
                 .build();
-        this.applicationRepository.save(application);
+        this.historyRepository.save(history);
+
         return new ResponseEntity<>(ResponseDto.res(
                 HttpStatus.CREATED,
                 "Created"
