fbd35dfff10a30060bae8f811b563c4a1301c0e1
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-11 21:51:23 +0900

    feat: 지원서 get 요청 시 파트를 Enum이 아닌 String으로 받게 됨에 따른 함수 추가 - #34
    - findBy 함수로 String에서 Part 추출

--- src/main/java/org/mjulikelion/bagel/model/Part.java
+++ src/main/java/org/mjulikelion/bagel/model/Part.java
@@ -1,5 +1,14 @@
 package org.mjulikelion.bagel.model;
 
 public enum Part {
-    SERVER, WEB
+    SERVER, WEB;
+
+    public static Part findBy(String part) {
+        for (Part p : Part.values()) {
+            if (p.name().equals(part)) {
+                return p;
+            }
+        }
+        return null;
+    }
 }
