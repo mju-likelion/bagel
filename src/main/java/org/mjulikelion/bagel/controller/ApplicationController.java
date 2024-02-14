feebe6906f1edacd5178ed49d6020d9005facdc3
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-15 04:35:15 +0900

    feat: 파일 저장 API 추가 - #4

--- src/main/java/org/mjulikelion/bagel/controller/ApplicationController.java
+++ src/main/java/org/mjulikelion/bagel/controller/ApplicationController.java
@@ -1,32 +1,29 @@
 package org.mjulikelion.bagel.controller;
 
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.mjulikelion.bagel.dto.request.ApplicationSaveDto;
 import org.mjulikelion.bagel.dto.response.ResponseDto;
 import org.mjulikelion.bagel.dto.response.application.ApplicationGetResponseData;
+import org.mjulikelion.bagel.dto.response.application.FileSaveResponseData;
 import org.mjulikelion.bagel.service.application.ApplicationCommandService;
 import org.mjulikelion.bagel.service.application.ApplicationQueryService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mjulikelion.bagel.util.annotaion.file.introduce.IntroduceFileConstraint;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("application")
 public class ApplicationController {
     private final ApplicationQueryService applicationQueryService;
     private final ApplicationCommandService applicationCommandService;
-
-    @Autowired
-    public ApplicationController(ApplicationQueryService applicationQueryService,
-                                 ApplicationCommandService applicationCommandService) {
-        this.applicationQueryService = applicationQueryService;
-        this.applicationCommandService = applicationCommandService;
-    }
 
     @GetMapping("/{part}")
     public ResponseEntity<ResponseDto<ApplicationGetResponseData>> getApplicationByPart(
@@ -36,7 +33,13 @@ public class ApplicationController {
 
     @PostMapping()
     public ResponseEntity<ResponseDto<Void>> saveApplication(
-            @RequestBody @Valid ApplicationSaveDto applicationSaveDto) {
+            @RequestPart @Valid ApplicationSaveDto applicationSaveDto) {
         return this.applicationCommandService.saveApplication(applicationSaveDto);
+    }
+
+    @PostMapping("/file")
+    public ResponseEntity<ResponseDto<FileSaveResponseData>> saveFile(
+            @IntroduceFileConstraint @RequestPart MultipartFile file) {
+        return this.applicationCommandService.saveFile(file);
     }
 }
