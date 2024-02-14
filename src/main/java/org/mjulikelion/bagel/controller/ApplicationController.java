9625ce5961011f2dc6634a92adc4d25529fd3597
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-15 04:51:52 +0900

    fix: Dto를 @RequestPart로 받고 있던 문제 수정 - #44
    - 이전 이슈 개발 중 발생한 실수

--- src/main/java/org/mjulikelion/bagel/controller/ApplicationController.java
+++ src/main/java/org/mjulikelion/bagel/controller/ApplicationController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +34,7 @@ public class ApplicationController {
 
     @PostMapping()
     public ResponseEntity<ResponseDto<Void>> saveApplication(
-            @RequestPart @Valid ApplicationSaveDto applicationSaveDto) {
+            @RequestBody @Valid ApplicationSaveDto applicationSaveDto) {
         return this.applicationCommandService.saveApplication(applicationSaveDto);
     }
 
