c9040afbf061d9f59dc7ac17f3a9d45d3e9960c1
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-15 04:33:12 +0900

    feat: AWS S3 사용을 위한 util 추가 - #4

---
+++ src/main/java/org/mjulikelion/bagel/util/s3/S3Service.java
@@ -0,0 +1,33 @@
+package org.mjulikelion.bagel.util.s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import java.io.IOException;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.s3.location}")
+    private String location;
+
+    public String saveFile(MultipartFile multipartFile) throws IOException {
+        String filename = location + UUID.randomUUID();
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(multipartFile.getSize());
+        metadata.setContentType(multipartFile.getContentType());
+
+        amazonS3.putObject(bucket, filename, multipartFile.getInputStream(), metadata);
+        return amazonS3.getUrl(bucket, filename).toString();
+    }
+}
