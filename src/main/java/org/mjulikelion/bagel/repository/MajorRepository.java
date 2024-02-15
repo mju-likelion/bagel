0ccf71af0739319d6ea95ab25dc10fbb65c151d7
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-15 22:57:41 +0900

    feat: findAll 시 sequence를 기준으로 정렬하는 함수 추가 - #50

--- src/main/java/org/mjulikelion/bagel/repository/MajorRepository.java
+++ src/main/java/org/mjulikelion/bagel/repository/MajorRepository.java
@@ -1,7 +1,9 @@
 package org.mjulikelion.bagel.repository;
 
+import java.util.List;
 import org.mjulikelion.bagel.model.Major;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MajorRepository extends JpaRepository<Major, String> {
+    List<Major> findAllByOrderBySequence();
 }
