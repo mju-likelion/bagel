d3fcb596af8ca083c4eba1dfc575c4c8a40b7d26
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-18 03:22:28 +0900

    feat: 일반적인 예외 상황에서 사용할 커스텀 Exception 클래스 구현 - #52
    - ErrorCode로 초기화, 에러 메시지는 ErrorCode에 있으므로, message는 사욯하지 않음

---
+++ src/main/java/org/mjulikelion/bagel/exception/CustomException.java
@@ -0,0 +1,13 @@
+package org.mjulikelion.bagel.exception;
+
+import lombok.Getter;
+import org.mjulikelion.bagel.errorcode.ErrorCode;
+
+@Getter
+public class CustomException extends RuntimeException {
+    protected ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+}
