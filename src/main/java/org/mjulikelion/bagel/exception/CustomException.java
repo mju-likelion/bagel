b408f97fbde583705dd687d49b2bd9f4fcd084cf
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-18 03:30:39 +0900

    feat: FileStorageException 상속을 위한 메서드 추가 - #52
    - FileStorageException은 에러 메시지를 출력해야 하므로

--- src/main/java/org/mjulikelion/bagel/exception/CustomException.java
+++ src/main/java/org/mjulikelion/bagel/exception/CustomException.java
@@ -10,4 +10,9 @@ public class CustomException extends RuntimeException {
     public CustomException(ErrorCode errorCode) {
         this.errorCode = errorCode;
     }
+
+    public CustomException(ErrorCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
 }
