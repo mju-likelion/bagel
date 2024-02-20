7134ecc5c3fe4bb1e8936390dbd4191dd9736f01
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-20 19:10:52 +0900

    feat: SlackBot을 통해 메시지를 보낼 때 사용할 채널 설정 - #65

---
+++ src/main/java/org/mjulikelion/bagel/util/slack/asset/SlackChannel.java
@@ -0,0 +1,15 @@
+package org.mjulikelion.bagel.util.slack.asset;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum SlackChannel {
+    FILE("#12기-지원-테스트-파일", "--- 파일이 업로드 되었습니다 ---"),
+    APPLICATION("#12기-지원-테스트-지원서", "--- 지원서가 업로드 되었습니다 ---"),
+    APPLY("#12기-지원-테스트-지원", "--- 새 지원 요청이 있습니다 ---");
+
+    private final String channelName;
+    private final String messageTitle;
+}
