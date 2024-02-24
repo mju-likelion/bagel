9473621ee58aae43cefcdd1eda9e4e3d07e4beaa
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-25 04:28:13 +0900

    feat: Rate Limit 적용 - #75

--- src/main/java/org/mjulikelion/bagel/controller/HealthController.java
+++ src/main/java/org/mjulikelion/bagel/controller/HealthController.java
@@ -1,10 +1,12 @@
 package org.mjulikelion.bagel.controller;
 
+import org.mjulikelion.bagel.util.annotaion.ratelimit.RateLimit;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
+    @RateLimit
     @GetMapping("/health")
     public String health() {
         return "ok";
