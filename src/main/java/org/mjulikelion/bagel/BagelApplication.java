15e72d8f112b4f5667f768bb979add671e726088
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-25 04:18:11 +0900

    feat: AspectJ 프록시를 활성화 - #75

--- src/main/java/org/mjulikelion/bagel/BagelApplication.java
+++ src/main/java/org/mjulikelion/bagel/BagelApplication.java
@@ -2,10 +2,12 @@ package org.mjulikelion.bagel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAspectJAutoProxy
 public class BagelApplication {
 
     public static void main(String[] args) {
