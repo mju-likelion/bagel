b1953f638975e14a5383e788b441c52bfa4d0154
Kim Daehyeon <daehyeon3351@gmail.com>
2024-03-01 00:05:15 +0900

    docs: 슬랙 채널명 변경 - #91

--- src/main/java/org/mjulikelion/bagel/util/slack/asset/SlackChannel.java
+++ src/main/java/org/mjulikelion/bagel/util/slack/asset/SlackChannel.java
@@ -6,9 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum SlackChannel {
-    FILE("#12기-지원사이트-파일", "--- 파일이 업로드 되었습니다 ---"),
-    APPLICATION("#12기-지원사이트-지원서", "--- 지원서가 업로드 되었습니다 ---"),
-    APPLY("#12기-지원사이트-지원", "--- 새 지원 요청이 있습니다 ---");
+    FILE("#12기-지원-파일", "--- 파일이 업로드 되었습니다 ---"),
+    APPLICATION("#12기-지원-지원서", "--- 지원서가 업로드 되었습니다 ---"),
+    APPLY("#12기-지원-지원", "--- 새 지원 요청이 있습니다 ---");
 
     private final String channelName;
     private final String messageTitle;
