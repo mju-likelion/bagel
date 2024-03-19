b5cc4c8cbd570cc02e7ce4910031e0572c514973
Kim Daehyeon <daehyeon3351@gmail.com>
2024-03-20 01:33:47 +0900

    refactor: StringRedisTemplate 사용에 따른 기존 코드 제거 - #101
    - redis는 기본적으로 StringRedisTemplate을 제공하므로, config에는 작성 필요 없음

--- src/main/java/org/mjulikelion/bagel/config/RedisConfig.java
+++ src/main/java/org/mjulikelion/bagel/config/RedisConfig.java
@@ -1,14 +1,10 @@
 package org.mjulikelion.bagel.config;
 
-import org.mjulikelion.bagel.dto.response.application.ApplicationGetResponseData;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -20,16 +16,5 @@ public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(redisHost, redisPort);
-    }
-
-    @Bean
-    public RedisTemplate<String, ApplicationGetResponseData> redisTemplateForApplicationGetResponseData(
-            RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, ApplicationGetResponseData> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(
-                new Jackson2JsonRedisSerializer<ApplicationGetResponseData>(ApplicationGetResponseData.class));
-        return redisTemplate;
     }
 }
