4cb8627d4730fd8545a5de6ab6f6e69b707f1b2e
Kim Daehyeon <daehyeon3351@gmail.com>
2024-03-28 07:13:50 +0900

    feat: RedisTemplate<K, V> 추가 - #101
    - Key와 Value 모두 Generic으로 함
    - 재사용성이 좋음

--- src/main/java/org/mjulikelion/bagel/config/RedisConfig.java
+++ src/main/java/org/mjulikelion/bagel/config/RedisConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -16,5 +18,14 @@ public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+
+    @Bean
+    public <K, V> RedisTemplate<K, V> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<K, V> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
     }
 }
